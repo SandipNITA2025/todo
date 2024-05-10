eea508578e05fd5cb0768c44699789eef3d816de
SandipNITA2025 <sarkarsandip7910@gmail.com>
2024-05-10 20:04:09 +0530

    vercel.json

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,10 @@
+import { LoginForm } from '@/components/component/login-form'
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='w-full h-screen flex items-center justify-center'><LoginForm/></div>
+  )
+}
+
+export default Login
---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,10 @@
+import { SignupForm } from '@/components/component/signup-form'
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <div className='w-full h-screen flex items-center justify-center'><SignupForm/></div>
+  )
+}
+
+export default Signup
